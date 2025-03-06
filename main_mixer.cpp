31a21c94def23d22a113b47ef2f0196717b96eaa
nephros <nemo@JollaC2>
2025-03-06 10:41:24 +0100

    Mixer: use new Mix_OpenAudio

--- main_mixer.cpp
+++ main_mixer.cpp
@@ -50,6 +50,8 @@ class SDL3TestApplicationMixer : public SDL3TestApplication {
 
     public:
         bool mix_opened;
+    protected:
+        const SDL_AudioSpec default_spec = { MIX_DEFAULT_FORMAT, 2, MIX_DEFAULT_FREQUENCY };
 };
 
 
@@ -69,7 +71,7 @@ on_audio_resource_acquired(audioresource_t *audio_resource, bool acquired, void 
 
     if (acquired && !app->mix_opened) {
         fprintf(stderr, "Audio resource acquired.\n");
-        int result = Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024);
+        int result = Mix_OpenAudio(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, default_spec);
         if (result == -1) {
             printf("Mix_OpenAudio: %s\n", SDL_GetError());
             exit(1);
